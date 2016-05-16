bbe2e45e3af33dfe4f70b3d87c0bb18bedae9ffa
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-05-16 19:46:19 -0400

    Change the Home pages title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -29,5 +29,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
-
 end
