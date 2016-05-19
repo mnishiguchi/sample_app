dcbcf16b5178885998839cdf1799933014b43539
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-05-18 21:41:18 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "A user was successfully created"
-      redirect_to user_path(@user)
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render "new"
     end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation' # The error explanation box.
+    assert_select 'div.has-error'         # Fields with errors.
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      # NOTE: post_via_redirect arranges to follow the redirect after submission.
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert_not flash.empty?  # The flash is NOT empty.
+  end
+end
